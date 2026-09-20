4a45ed8a553d1869ecc2f75971513964eaac193e
Adityaj2902 <adityajofficial909@gmail.com>
2026-09-20 22:54:48 +0530

    Enhance user route handling by adding GET, POST, and DELETE methods in app.js. Update response messages for clarity.

--- src/app.js
+++ src/app.js
@@ -8,10 +8,23 @@ const app = express(); // Create an instance of the express application
 // });
 
 //To give control over the particular route
-
 app.use("/users", (req, res) => {
-    res.send("Hello From the Users Routen Ankusha");
+    res.send("Hello From the Users Router Handling HA HA HA  HA ");
 });
+
+// This will only handle the get Call to /User
+app.get("/users", (req, res) => {
+    res.send("Hello From the Users Ankusha Sabharwal");
+});
+
+app.post("/users", (req, res) => {
+    res.send("Hello From the Users Ankusha Sabharwa and Saved in the Database");
+});
+
+app.delete("/users", (req, res) => {
+    res.send("Hello From the Users Ankusha Sabharwal and Deleted from the Database");
+});
+
 
 // we have to call listen method to start the server
 app.listen(3000, () => {
