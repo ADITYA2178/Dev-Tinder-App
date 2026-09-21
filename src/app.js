df53ff52a05c552a07eb11c62baf462212693d78
Adityaj2902 <adityajofficial909@gmail.com>
2026-09-21 23:40:18 +0530

    What is the need of Middleware ?

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require("express");
 
 const app = express(); // Create an instance of the express application
 
-app.get("/users", (req, res, next) => {
-    // res.send("Hello From the Users Router Handling");
-    next();
+
+app.get("/admin/getAllData", (req, res) => {
+    //Logic of checking if the user is authenticated
+    const token = "xyz";
+    const isAuthenticated = token === "xyz";
+    if (!isAuthenticated) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+    res.send("All Data Sent");
 });
 
-app.get("/users",(req,res,next)=> {
-    res.send("Hello From the Users Router Handling 2");
-})
- 
+app.get("/admin/deleteUser", (req, res) => {
+    //Logic of checking if the user is authenticated
+    res.send("User Deleted");
+});
+
 // we have to call listen method to start the server
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
-
