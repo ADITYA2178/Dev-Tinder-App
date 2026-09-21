cba422d64efd96213e8c3d971712689fc959766f
Adityaj2902 <adityajofficial909@gmail.com>
2026-09-21 07:27:27 +0530

    Importance of "next()" function call in Multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,47 +2,30 @@ const express = require("express");
 
 const app = express(); // Create an instance of the express application
 
-// RequestHandler
-// app.use((req, res) => {
-//    res.send("Hello From the Sever");
-// });
-
-//To give control over the particular route
-app.use("/users", (req, res) => {
-    res.send("Hello From the Users Router Handling HA HA HA  HA ");
-});
-
-// This will only handle the get Call to /User
-app.get("/users", (req, res) => {
-    res.send("Hello From the Users Ankusha Sabharwal");
-});
-
-app.post("/users", (req, res) => {
-    res.send("Hello From the Users Ankusha Sabharwa and Saved in the Database");
-});
-
-app.delete("/users", (req, res) => {
-    res.send("Hello From the Users Ankusha Sabharwal and Deleted from the Database");
-});
-
-//We can make the option in routing in by using "?"
-app.get("/a{b}c" , (req, res) => {
-    res.send("Hello From the Users Ankusha Sabharwal By using Param Routing Optional");
-});
-
-app.get("/checkUsername", (req, res) => {
-    console.log(req.query.userId);
-    res.send("Hello From the Users Ankusha Sabharwal and Checked the Username");
-});
-
-app.get("/checkUsername/:username", (req, res) => {
-    console.log(req.params);
-    res.send("Hello From the Users Ankusha Sabharwal and Checked the Username");
-});
-
-
-
-
+app.use("/users", (req, res,next) => {
+    res.send("Hello From the Users Router Handling");
+    next();
+},
+(req, res) => {
+    res.send("Hello From the Users Router Handling 2");
+},
+(req, res) => {
+    res.send("Hello From the Users Router Handling 3");
+},
+(req, res) => {
+    res.send("Hello From the Users Router Handling 4");
+},
+(req, res) => {
+    res.send("Hello From the Users Router Handling 5");
+},
+(req, res) => {
+    res.send("Hello From the Users Router Handling 6");
+},
+(req, res) => {
+    res.send("Hello From the Users Router Handling 7");
+},
+);
+ 
 // we have to call listen method to start the server
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
