505dac7f5b26915781e523a32aa2774f42abc1aa
Adityaj2902 <adityajofficial909@gmail.com>
2026-09-21 07:39:16 +0530

    Another way of Multiple Nested Route handler instead of nested we can write one by one

--- src/app.js
+++ src/app.js
@@ -2,29 +2,14 @@ const express = require("express");
 
 const app = express(); // Create an instance of the express application
 
-app.use("/users", (req, res,next) => {
-    res.send("Hello From the Users Router Handling");
+app.get("/users", (req, res, next) => {
+    // res.send("Hello From the Users Router Handling");
     next();
-},
-(req, res) => {
+});
+
+app.get("/users",(req,res,next)=> {
     res.send("Hello From the Users Router Handling 2");
-},
-(req, res) => {
-    res.send("Hello From the Users Router Handling 3");
-},
-(req, res) => {
-    res.send("Hello From the Users Router Handling 4");
-},
-(req, res) => {
-    res.send("Hello From the Users Router Handling 5");
-},
-(req, res) => {
-    res.send("Hello From the Users Router Handling 6");
-},
-(req, res) => {
-    res.send("Hello From the Users Router Handling 7");
-},
-);
+})
  
 // we have to call listen method to start the server
 app.listen(3000, () => {
