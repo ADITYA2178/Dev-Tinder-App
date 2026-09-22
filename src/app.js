44bc0ea689b5fc6596e6bbfbc4f71f129678d4c5
Adityaj2902 <adityajofficial909@gmail.com>
2026-09-22 08:20:39 +0530

    Error Handling Implementation

--- src/app.js
+++ src/app.js
@@ -2,24 +2,26 @@ const express = require("express");
 
 const app = express(); // Create an instance of the express application
 
-const { adminAuth, userAuth } = require("./middleware/auth");
 
+// All Try to use try and catch method for error handling
 
-app.use("/admin",adminAuth);
-app.use("/user",userAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All Data Sent");
+app.get("/getUserData", (req, res) => {
+ try{
+    throw new Error("User Data Not Found");
+    res.send("User Data Sent");
+ }
+ catch(error){
+    res.status(500).send("Some Error Contact Support Team");
+    next(error);
+ }
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-    //Logic of checking if the user is authenticated
-    res.send("User Deleted");
-});
 
-app.get("/user/getAllData",userAuth, (req, res) => {
-    res.send("All Data Sent");
-});
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something Went Wrong");
+    }
+}) 
 
 // we have to call listen method to start the server
 app.listen(3000, () => {
