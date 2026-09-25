8866605203e74389736d03a342aef8bec81f5ac6
Adityaj2902 <adityajofficial909@gmail.com>
2026-09-25 12:38:06 +0530

    Deleted User By Id

--- src/app.js
+++ src/app.js
@@ -19,6 +19,55 @@ app.post("/signup", async (req, res) => {
 });
 
 
+// Get User by email 
+app.get("/user",async(req,res)=>{
+    const firstName=req.body.firstName;
+    const user=await User.find({firstName});
+
+    try{
+        if (!user) {
+            res.status(404).send("User not found");
+          } else { 
+            res.send(user);
+        }
+    }
+    catch(error){
+        res.status(500).send("Internal server error");
+    }
+})
+
+//Feed Api
+app.get("/feed",async(req,res)=>{
+    const user=await User.find({});
+    try{
+        if(user.length === 0){
+            res.status(404).send("No users found");
+        }else{
+            res.send(user);
+        }
+    }catch(error){
+        res.status(500).send("Internal server error");
+    }
+});
+
+
+// To DELETE THE USER BY FINDING THE ID 
+
+app.delete("/user/remove",async(req,res)=>{
+    const id=req.body.id;
+    const user=await User.findByIdAndDelete(id);
+    try{
+        if(!user){
+            res.status(404).send("User not found");
+        }else{
+            res.send("User deleted successfully");
+        }
+    }catch(error){
+        res.status(500).send("Internal server error");
+    }
+});
+ 
+
 // Here First we have established the connection to the database and then we have started the server
 connectDB().then(()=>{
     console.log("Database Connected Successfully");
